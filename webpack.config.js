081da7f87e06757c1951d7b08d91c6e4f3f26ebb
Wagadre, Anshul <anshul.wagadre@intel.com>
2022-07-11 10:32:30 +0530

    feat: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  // Where files should be sent once they are bundled
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.bundle.js",
+  },
+  // webpack 5 comes with devServer which loads in development mode
+  devServer: {
+    port: 4000,
+  },
+  // Rules of how webpack will take our files, complie & bundle them for the browser
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(png|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new ModuleFederationPlugin({
+      name: "MFE1",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./src/App",
+      },
+    }),
+  ],
+};
